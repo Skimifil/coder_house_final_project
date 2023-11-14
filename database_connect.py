df827b41ed1aa6621d56d35bc1ffbe3b0ef6fd88
skimifil <rafaelpinheiro_@hotmail.com>
2023-11-13 23:18:58 -0300

    Esqueci do arquivo de conexão com o banco.

---
+++ database_connect.py
@@ -0,0 +1,21 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from configs import MONGODB_CONFIG
+
+
+def connect_mongo():
+
+    mongo_user = MONGODB_CONFIG['MONGODBUSER']
+    mongo_pass = MONGODB_CONFIG['MONGODBPASSWORD']
+    uri = f"mongodb+srv://{mongo_user}:{mongo_pass}@rpcarvalho.tffavxg.mongodb.net/?retryWrites=true&w=majority"
+
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        #print("Pinged your deployment. You successfully connected to MongoDB!")
+        return client
+    except Exception as e:
+        print(e)
